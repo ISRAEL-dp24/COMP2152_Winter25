0e341a929f0307b5fd9585716f29b26af7511c6a
ISRAEL-dp24 <dolapoosunkoya5@gmail.com>
2025-01-19 15:11:43 -0500

    update repo

---
+++ Week01/lab1.py
@@ -0,0 +1,26 @@
+# Sample Coding Questions 01 Week 01
+# First Name: Israel
+# Last Name: Osunkoya
+
+# Defining Variables:
+array = [1, 4, 7, 9]
+
+# Order of Operations:
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Non-Bracketed Version:
+e = a - b ** c // d + a % c
+# Fully-Bracketed Version:
+e = (a - (((b ** c) // d) + (a % c)))
+
+# Formatting:
+temperature = 32.6
+print(f"The temperature today is: {temperature} degrees Celsius")
+
+# Common Functions:
+userAge = int(input("Enter your age: "))
+userAge += 22
+print(f"Now showing the shop items filtered by age: {userAge}")
